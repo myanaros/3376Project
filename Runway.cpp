45fe42c4f3f4da512954edbf45cb5c9b1e50fbff
Mark Yanaros <mark.yanaros@gmail.com>
2015-04-03 15:18:45 -0500

    my added prototypes and incomplete implementations of methods.

--- Runway.cpp
+++ Runway.cpp
@@ -2,3 +2,51 @@
 
 // Project includes
 #include "Runway.h"
+
+int Runway::landing_duration()
+{
+    return landing_duration_;
+}
+
+Runway::set_landing_duration(int landing_in)
+{
+    landing_duration_ = landing_in;
+}
+
+int Runway::takeoff_duration_()
+{
+    return takeoff_duration_;
+}
+
+void Runway::set_takeoff_duration_(int takeoff_in)
+{
+    takeoff_duration_ = takeoff_in;
+}
+
+int Runway::busy_until()
+{
+    return busy_until_;
+}
+
+void Runway::set_busy_until(int busy_in)
+{
+    busy_until_ = busy_in;
+}
+
+//TODO
+bool Runway::isClear()
+{
+    return false;
+}
+
+//TODO
+void Runway::doLanding()
+{
+
+}
+
+//TODO
+void Runway::doTakeoff()
+{
+
+}
--- Runway.h
+++ Runway.h
@@ -2,6 +2,21 @@
 #define __CS3376_RUNWAY_H__
 
 class Runway {
+    public:
+        int landing_duration();
+        void set_landing_duration(int);
+        int takeoff_duration_();
+        void set_takeoff_duration_(int);
+        int busy_until();
+        void set_busy_until(int);
+
+        bool isClear();
+        void doLanding();
+        void doTakeoff();
+    private:
+        int landing_duration_;
+        int takeoff_duration_;
+        int busy_until_;
 };
 
 #endif
