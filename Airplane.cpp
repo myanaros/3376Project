37e4f5b9554135740e3296c355b5539f2267db8a
Mark Yanaros <mark.yanaros@gmail.com>
2015-04-03 20:16:41 -0500

    my added comments.

--- Airplane.cpp
+++ Airplane.cpp
@@ -3,27 +3,31 @@
 // Project includes
 #include "Airplane.h"
 
+//sets the start time
 void Airplane::set_start_time(int start_in)
 {
     start_time_ = start_in;
 }
 
+//gets the start time
 int Airplane::get_start_time()
 {
     return start_time_;
 }
 
+//sets the crash time
 void Airplane::set_crash_time(int crash_in)
 {
     crash_time_ = crash_in;
 }
 
+//gets the crash time
 int Airplane::get_crash_time()
 {
     return crash_time_;
 }
 
-//TODO
+//TODO determines whether or not the plane has crashed
 bool Airplane::hasCrashed()
 {
     return false;
