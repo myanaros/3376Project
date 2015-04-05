7db017adbb0447827a62566c626f7c04c25629c8
Travis Millican <travisjmillican@gmail.com>
2015-04-05 17:41:10 -0500

    constructor/destructor, standardized names, missing args

--- Airplane.h
+++ Airplane.h
@@ -5,13 +5,22 @@ class Airplane {
     private:
         int start_time_;
         int crash_time_;
-    public:
-        void set_start_time(int);
-        int get_start_time();
-        void set_crash_time(int);
-        int get_crash_time();
 
-        bool hasCrashed();
+    public:
+        // Constructors/Destructors
+        Airplane(const int cur_time, const int max_time);
+        ~Airplane() {};
+
+        // Accessors
+        int start_time();
+        int crash_time();
+
+        // Mutators
+        void set_start_time(const int start_time);
+        void set_crash_time(const int crash_time);
+
+        // Other
+        bool hasCrashed(const int cur_time);
 };
 
 #endif
