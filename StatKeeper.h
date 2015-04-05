c8fab093a8c59ba05f333b59e381a99d3855e900
Travis Millican <travisjmillican@gmail.com>
2015-04-05 18:14:09 -0500

    constructor/destructor, naming convention, const correctness, added printStats()

--- StatKeeper.h
+++ StatKeeper.h
@@ -3,34 +3,36 @@
 
 class StatKeeper {
 	private:
-	//Set up class variables
-		int total_time_;
-		int total_takeoffs_;
-		int total_landings_;
+		int total_sim_time_;
 		int total_crashes_;
-		int average_takeoff_;
-		int average_landing_;
+		int total_landings_;
+		int total_takeoffs_;
+        int total_landing_time_;
+        int total_takeoff_time_;
 	
 	public:
-	//Constructor
-		StatKeeper();
-	//Class accessor methods.
-		int get_total_time();
-		int get_total_takeoffs();
-		int get_total_landings();
-		int get_total_crashes();
-		int get_average_takeoff();
-		int get_average_landing();
+        // Constructors /Destructors
+		StatKeeper(const int start_time, const int end_time);
+        ~StatKeeper();
 
-	//Class mutator methods.
-		void set_total_time(int, int);
-		void set_total_takeoffs(int);
-		void set_total_landings(int);
-		void set_total_crashes(int);
-		void set_average_takeoff(int);
-		void set_average_landing(int);
+        // Accessors
+		int total_sim_time();
+		int total_crashes();
+		int total_landings();
+		int total_takeoffs();
+        int total_landing_time();
+        int total_takeoff_time();
 
+        // Mutators
+		void set_total_sim_time(const int total_sim_time);
+		void set_total_crashes(const int total_crashes);
+		void set_total_landings(const int total_landings);
+		void set_total_takeoffs(const int total_takeoffs);
+		void set_total_landing_time(const int total_landing_time);
+		void set_total_takeoff_time(const int total_takeoff_time);
 
+        // Other
+        void printStats();
 };
 
 #endif
