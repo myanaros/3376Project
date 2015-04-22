173f52d71aefacd7241c3527a27124bf8a664c02
Mark Yanaros <mark.yanaros@gmail.com>
2015-04-21 19:46:18 -0500

    my merging airsim versions

--- airsim.cpp
+++ airsim.cpp
@@ -6,16 +6,48 @@
 #include "Queue.h"
 #include "Runway.h"
 #include "StatKeeper.h"
-
 #include <iostream>
-#include <unistd.h>
+#include <string>
 #include <stdlib.h>
-
-using namespace std;
-
-int main(int argc, char **argv) {
+#include <unistd.h>
+int main(int argc, char *argv[]) {
     //Needed to seed rand() calls at the beginning of the program.
     srand(time(NULL));
+
+	if (argc < 7)
+	{
+		std::cout << "Not enough arguments passed. Needs 7 arguments. Terminating simulation.\n";
+		return 1;
+	}
+	if (argc > 8)
+	{
+		std::cout << "Too many arguments. Requires 7 arguments. Terminating simulation. \n";
+	}
+	int time_to_land;
+	int time_for_takeoff;
+	double plane_enter_landing;
+	double plane_enter_takeoff;
+	int start_time;
+	int end_time;
+	int crash_time;
+	for (int i = 1; i <= argc; i++)
+	{
+		if(i==1)
+		time_to_land = atoi(argv[i]);
+		else if (i==2)
+		time_for_takeoff = atoi(argv[i]);
+		else if (i==3)
+		plane_enter_landing = atof(argv[i]);
+		else if (i==4)
+		plane_enter_takeoff = atof(argv[i]);
+		else if (i==5)
+		start_time = atoi(argv[i]);
+		else if (i==6)
+		end_time = atoi(argv[i]);
+		else if (i==7)
+		crash_time = atoi(argv[i]);
+	}
+//	std::cout << time_to_land << time_for_takeoff << plane_enter_landing << plane_enter_takeoff << start_time << end_time << crash_time;
 
     return 0;
 }
